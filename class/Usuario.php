b89907afa39a37069dc9751e0759e7b55e184a71
Johannes Pablo Rey <johannespablorey@gmail.com>
2022-03-14 12:39:05 -0300

    inserir e alterar

--- class/Usuario.php
+++ class/Usuario.php
@@ -8,36 +8,51 @@ class Usuario {
     private $dtcadastro;
 
     public function getIdusuario(){
+
         return $this->idusuario;
+
     }
 
-    public function setIdusuario($value) {
+    public function setIdusuario($value){
+
         $this->idusuario = $value;
+
     }
 
     public function getDeslogin(){
+
         return $this->deslogin;
+
     }
 
-    public function setDeslogin($value) {
+    public function setDeslogin($value){
+
         $this->deslogin = $value;
+
     }
 
     public function getDessenha(){
 
         return $this->dessenha;
+
     }
 
-    public function setDessenha($value) {
+    public function setDessenha($value){
+
         $this->dessenha = $value;
+
     }
 
     public function getDtcadastro(){
+
         return $this->dtcadastro;
+
     }
 
-    public function setDtcadastro($value) {
+    public function setDtcadastro($value){
+
         $this->dtcadastro = $value;
+
     }
 
     public function loadById($id){
@@ -45,18 +60,14 @@ class Usuario {
         $sql = new Sql();
 
         $results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
+
             ":ID"=>$id
+
         ));
 
-        // if (isset($results[0])); da pra usar assim tbm, quer dizer, se existir, puxar os itens
-        if (count($results) > 0){
+        if (count($results) > 0){   // if (isset($results[0])); da pra usar assim tbm, quer dizer, se existir, puxar os itens
 
-            $row = $results[0];
-
-            $this->setIdusuario($row['idusuario']);
-            $this->setDeslogin($row['deslogin']);
-            $this->setDessenha($row['dessenha']);
-            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+            $this->setData($results[0]);
 
         }
 
@@ -75,7 +86,9 @@ class Usuario {
         $sql = new Sql();
 
         return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+
             ':SEARCH'=>"%".$login."%"
+
         ));
 
     }
@@ -85,27 +98,71 @@ class Usuario {
         $sql = new Sql();
 
         $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+
             ":LOGIN"=>$login,
-            ":PASSWORD"=>$password
+            ":PASSWORD"=>$password,
+
         ));
+        
+        if (count($results) > 0) {   // if (isset($results[0])); da pra usar assim tbm, quer dizer, se existir, puxar os itens
 
-        // if (isset($results[0])); da pra usar assim tbm, quer dizer, se existir, puxar os itens
-        if (count($results) > 0){
-
-            $row = $results[0];
-
-            $this->setIdusuario($row['idusuario']);
-            $this->setDeslogin($row['deslogin']);
-            $this->setDessenha($row['dessenha']);
-            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+            $this->setData($results[0]);
 
         } else {
 
             throw new Exception("Login e/ou senha inválidos.");
             
+        }
+
+    }
+
+    public function setData($data){
+
+        $this->setIdusuario($data['idusuario']);
+        $this->setDeslogin($data['deslogin']);
+        $this->setDessenha($data['dessenha']);
+        $this->setDtcadastro(new DateTime($data['dtcadastro']));
+
+    }
+
+    public function insert(){
+
+        $sql = new Sql();
+
+        $results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+
+            ':LOGIN'=>$this->getDeslogin(),
+            ':PASSWORD'=>$this->getDessenha()
+
+        ));
+
+        if (count($results) > 0) {
+
+            $this->setData($results[0]);
 
         }
 
+    }
+
+    public function update($login, $password){
+
+        $this->setDeslogin($login);
+        $this->setDessenha($password);
+
+        $sql = new Sql();
+
+        $sql->Sql("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(
+            ':LOGIN'=>$this->getDeslogin(),
+            ':PASSWORD'=>$this->getDessenha(),
+            ':ID'=>$this->getIdusuario()
+        ));
+
+    }
+
+    public function __construct($login = "", $password = "")
+    {
+        $this->setDeslogin($login);
+        $this->setDessenha($password);
     }
 
     public function __toString()
--- index.php
+++ index.php
@@ -3,34 +3,63 @@
 require_once("config.php");
 
 //      Carrega todos os usuários
+
 // $sql = new Sql();
+
 // $usuarios = $sql->select("SELECT * FROM tb_usuarios");
+
 // echo json_encode($usuarios);
 
 //////////////////////////////////////////////////////////
 
 //      Carrega um usuário
+
 // $root = new Usuario();
+
 // $root->loadById(3);
+
 // echo $root;
 
 //////////////////////////////////////////////////////////
 
 //      Carrega uma lista de usuários
+
 // $lista = Usuario::getList();
+
 // echo json_encode($lista);
 
-/////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
 
 //      Carrega uma lista de usuários buscando pelo login
 // $search = Usuario::search("jo");
+
 // echo json_encode($search);
 
-/////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
 
 //      Carrega um usuário usando o login e a senha
+
+// $usuario = new Usuario();
+
+// $usuario->login("root","!@#$");
+
+// echo $usuario;
+
+//////////////////////////////////////////////////////////
+
+//      Criando um usuário novo
+
+// $aluno = new Usuario("aluno", "@lun0");
+
+// $aluno->insert();
+
+// echo $aluno;
+
 $usuario = new Usuario();
-$usuario->login("root","!@#$");
+
+$usuario->loadById(12);
+
+$usuario->update("professor", "senhaprofessor");
 
 echo $usuario;
 
